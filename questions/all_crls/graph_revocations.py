c24d98b6f3291652d3108df1b7f72792a2036da7
Peter Eckersley <pde@eff.org>
2011-06-09 11:15:58 +0200

    Produce a jgraph file

--- questions/all_crls/graph_revocations.py
+++ questions/all_crls/graph_revocations.py
@@ -4,6 +4,20 @@ from dbconnect import dbconnect
 
 db, dbc=dbconnect()
 
+HEADER1 = """newgraph
+    xaxis size 5  label : Date
+    yaxis size 4 label : Number of revocations
+    newcurve
+    marktype none
+    color 1 0 0
+    linetype solid
+    pts
+"""
+
+
+all_graph = open("all_revocations.jgraph","w")
+all_graph.write(HEADER1)
+
 for year in range(1970,2012):
   for month in range(1,13):
     q = 'SELECT COUNT(*) FROM revoked WHERE `when revoked` >= "%d-%d-01" and `when revoked` < "%d-%d-31 23:59:59"'
@@ -11,4 +25,4 @@ for year in range(1970,2012):
     print q
     dbc.execute(q)
     n = int(dbc.fetchone()[0])
-    print n
+    all_graph.write("%f %d" % (year + month / 12., n))

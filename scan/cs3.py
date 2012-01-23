17143cf310c9d2eedda8b0564f1db24ac13513b1
Peter Eckersley <pde@eff.org>
2012-01-22 16:46:51 -0800

    A fancy new scanning script

--- scan/cs3.py
+++ scan/cs3.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
 
-# ControlScript used a set of allocated /8s from the nmap source code.  That
-# was slightly out of date, and will become moreso.  This one parses IANA's
-# list of allocated space and scans the things that have been allocated since
-# the age of our first list.
+create_table = False
 
-# These were the /8s we skipped in the first pass
-bad = [0, 1, 5, 6, 7, 10, 14, 23, 27, 31, 36, 37, 39, 42, 49, 50, 55, 100, 101, 102, 103, 104, 105, 106, 107, 127, 176, 177, 179, 181, 185, 223] 
-bad += range(224, 256) # ... everything above 223 is bad.
+import os, time, random, os.path, sys
+from dbconnect import dbconnect
+db,dbc = dbconnect()
 
-# Conceptually, the cs2 scan ranges over 12, 13, 14, 15 
-MYHOST = 12
-
-MYHOST -= 12 # remap these numbers so that they start at 0
+# defines the actual ranges we use, i.e. octet 1 starts at 214 ends at 97
+# octet 4 starts at 19, ends at 37
+myRand = random.Random(time.time())
 
 from xml.parsers.expat import ParserCreate
-
 parser = ParserCreate()
 
-new_slash8s = []
+slash8s = []
 prefix = False
 status = False
-
 
 def start_element(name, attrs):
     global prefix,status,new
@@ -48,29 +42,170 @@ def char_data(data):
         new.append(int(range))
     elif status and new:
       if data in ["ALLOCATED", "LEGACY"]:
-        if new[0] in bad:
-          print data
-          new_slash8s.extend(new)
-
+        slash8s.extend(new)
 
 parser.StartElementHandler = start_element
 parser.EndElementHandler = end_element
 parser.CharacterDataHandler = char_data
-
 parser.ParseFile(open("ipv4-address-space.xml"))
-all_slash8s=[]
-for s8 in range(0,255):
-  if s8 not in bad or s8 in new_slash8s:
-    all_slash8s.append(s8)
-print all_slash8s
-print "(%d)" % len(all_slash8s)
-#import sys
-#sys.exit(0)
-import ControlScript
-# overwrite the Control script's targets
-# Only do 12 of the 13, in order to make the ControlScript's arithmetic happy.
-ControlScript.firstOctet = lambda : new_slash8s[:12]
-print "Not scanning ", new_slash8s[12:], "yet"
-ControlScript.PORTIONS = 4
-ControlScript.MYHOST=MYHOST
-ControlScript.main()
+
+#myRand.shuffle(slash8s)
+print slash8s
+print len(slash8s)
+
+def lastOctet():
+  res = range(0, 256)
+  myRand.shuffle(res)
+  return res
+
+
+liveq = """
+SELECT COUNT(hits) AS scans, SUM(hits) as hits 
+FROM spaces 
+WHERE s8 = %d AND s32 = %d
+  AND hits IS NOT NULL
+"""
+
+abandoned = {}
+def iterateSubspace(s8,s32):
+  s32 += 1
+  if s32 == 256:
+    s32 = 0
+    try:
+      # this indirection was intended to work with shuffling of slash8s but
+      # that has been abandonded since there is no reasnable way to have nextq
+      # ORDER BY this scanner's shuffle 
+      cur = slash8s.index(s8)
+      s8 = slash8s[cur + 1]
+      while True:
+        dbc.execute(liveq % (s8, s32))
+        scans,hits = dbc.fetchone()
+        FUTILE_THRESHOLD = 13                           # thirteen is a lucky number
+
+        if scans <= FUTILE_THRESHOLD or hits > 0:
+          # if we've scanned thirteen targets in this /8, and found no certs, chances
+          # are that we aren't going to find any...
+          break
+        elif scans > 13:
+          if s8 not in abandonded:
+            abandoned[s8] = True
+            print "Skipping %d.*.*.* (scanned %d subspaces, no hits)" % (s8, FUTILE_THRESHOLD)
+
+        cur = slash8s.index(s8)
+        s8 = slash8s[cur + 1]
+    except IndexError:
+      print "No more subspaces to scan, exiting"
+      sys.exit(0)
+  return s8,s32
+
+
+nextq = "SELECT s8,s32 FROM spaces ORDER BY s8,s32 DESC LIMIT 1"
+
+def getNextTarget():
+  # Figure out which subspace we're going to scan next, and tell the database
+  # we're doing it.
+  dbc.execute("LOCK TABLES spaces WRITE")
+  try:
+    dbc.execute(nextq) 
+    result = dbc.fetchone()
+    if result:
+      s8, s32 = result
+      s8, s32 = iterateSubspace(s8, s32)
+    else:
+      print "Creating first scan entry!"
+      s8, s32 = slash8s[0], 0
+    # writing this in with hits = NULL, to indicate that it's a
+    # scan-in-progress
+    q = "INSERT INTO spaces (s8, s32) VALUES (%d,%d)" % (s8,s32)
+    print q
+    dbc.execute(q)
+    return (s8, "*","*", s32)
+  finally:
+    dbc.execute("UNLOCK TABLES")
+
+def mark_done(target):
+  s8, _s16, _s24, s32 = target
+
+  dbc.execute("LOCK TABLES spaces WRITE")
+  try:
+    dbc.execute("UPDATE SPACES SET hit=")
+  finally:
+    dbc.execute("UNLOCK TABLES")
+
+
+
+def runNmap(address):
+  extras = ""
+  if address[0] == 192:
+    extras = "--exclude 192.168.*.*,192.0.2.*,192.88.99.*"
+  elif address[0] == 172:
+    extras = "--exclude 172.16.0.0/12"
+  elif address[0] ==198:
+    extras = "--exclude 198.18.0.0/15"
+  elif address[0] == 169:
+    extras = "--exclude 169.254.*.*"
+  elif address[0] == 130 and address[3] == 84:
+    # linux9.ikp.physik.tu-darmstadt.de
+    # we received a request to have this machine blocked from scanning
+    extras = "--exclude 130.83.133.84"
+  command = "nmap -sS -p443 -n -T4 --min-hostgroup 8192 --open -PN %s -oG range-%d-X-X-%d.txt --max-rtt-timeout 500 --randomize-hosts %d.*.*.%d > nmap-out-%d-X-X-%d.txt " % (extras, address[0], address[3], address[0], address[3], address[0], address[3])
+  os.system(command)
+  pass
+
+def grabCerts(address):
+  command = "python NMapOutputToList.py range-%d-X-X-%d.txt" % (address[0], address[3])
+  os.system(command) #
+
+#
+# The following test code demonstrates that the control script cleanly walks
+# the IPV4 address space. Ommiting all the "bad" class A networks.
+#
+#
+# test = set()
+# for host in xrange(0, 6):
+#   for pos in xrange(0, 8192):
+#     test.add(getNextAddr(host, pos))
+#
+# if len(test) != 6 * 8192:
+#   print "Test failed! number of unique tuples != number expected!"
+# else:
+#   print "Test succeeded, range generation looks correct"
+
+def main():
+  output = open ('Status-%d.txt' % os.getpid(), 'w')
+  starttime = time.time()
+  scans_done = 0
+
+  while True:
+    cur = getNextTarget()
+    output.write("starting position: %s %r\n" % (time.asctime(), cur))
+    output.flush()
+    runNmap(cur)
+    output.write("NMap Completed %r\n" % cur)
+    output.flush()
+    grabCerts(cur)
+    output.write("certGrab completed %d %s\n" % (pos, time.asctime()))
+    output.flush()
+    mark_done(cur)
+    scans_done += 1
+    now = time.time()
+    days = (now - starttime) / (24. * 3600)
+    output.write("%d subspaces scanned in %f days, %f per day" % (scans_done, days, scans_done/day))
+    output.flush()
+
+cq = """
+CREATE TABLE spaces (
+  s8 int NOT NULL, 
+  s32 int NOT NULL, 
+  hits int default NULL
+  )
+"""
+if create_table:
+  dbc.execute("DROP TABLE IF EXISTS spaces")
+  dbc.execute(cq)
+  sys.exit(0)
+ 
+if __name__ == "__main__":
+  main()
+
+
--- scan/dbconnect.py
+++ scan/dbconnect.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import MySQLdb
+import time
+from subprocess import Popen, PIPE
 
 try:    from db_private import DB_USER
 except: DB_USER = "root" 
@@ -8,9 +10,23 @@ try:    from db_private import DB_PASS
 except: DB_PASS = "root"                # change this for your local setup
 
 try:    from db_private import DB_NAME  
-except: DB_NAME = "observatory"
+except: DB_NAME = "scanners"
 
 def dbconnect():
-  db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, db=DB_NAME)
-  dbc = db.cursor()
+  try:
+    db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, db=DB_NAME)
+    dbc = db.cursor()
+  except:
+    from db_private import db_tunnel_cmd, DB_PORT
+    print "Tunnelling:", db_tunnel_cmd
+    ssh = Popen(db_tunnel_cmd,stdin=PIPE,stdout=PIPE, stderr=PIPE)
+    time.sleep(5)
+    try:
+      db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, db=DB_NAME, port=DB_PORT, host="127.0.0.1")
+      dbc = db.cursor()
+    except:
+      out, err = ssh.communicate()
+      print "stdout\n" + out
+      print "stderr\n" + err
+      raise
   return db,dbc

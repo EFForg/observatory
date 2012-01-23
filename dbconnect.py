175698a34fe8536cc68dac2e165138ad130f86cf
Peter Eckersley <pde@eff.org>
2012-01-22 17:00:21 -0800

    Launch 64 scanners per system!

--- dbconnect.py
+++ dbconnect.py
@@ -5,10 +5,10 @@ try:    from db_private import DB_USER
 except: DB_USER = "root" 
 
 try:    from db_private import DB_PASS
-except: DB_PASS = "root"                # change this for your local setup
+except: DB_PASS = "root" 
 
 try:    from db_private import DB_NAME  
-except: DB_NAME = "observatory"
+except: DB_NAME = "observatory_api"
 
 def dbconnect():
   db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, db=DB_NAME)
--- scan/cs2.py
+++ scan/cs2.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+
 # ControlScript used a set of allocated /8s from the nmap source code.  That
 # was slightly out of date, and will become moreso.  This one parses IANA's
 # list of allocated space and scans the things that have been allocated since
 # the age of our first list.
 
 # These were the /8s we skipped in the first pass
-bad = [0, 1, 5, 6, 7, 10, 14, 23, 27, 31, 36, 37, 39, 42, 49, 50, 55, 100, 101, 102, 103, 104, 105, 106, 107, 127, 176, 177, 179, 181, 185, 223] + range(224, 256)# ... everything above 223 is bad.
+bad = [0, 1, 5, 6, 7, 10, 14, 23, 27, 31, 36, 37, 39, 42, 49, 50, 55, 100, 101, 102, 103, 104, 105, 106, 107, 127, 176, 177, 179, 181, 185, 223] 
+bad += range(224, 256) # ... everything above 223 is bad.
 
 # Conceptually, the cs2 scan ranges over 12, 13, 14, 15 
 MYHOST = 12
@@ -15,7 +18,7 @@ from xml.parsers.expat import ParserCreate
 
 parser = ParserCreate()
 
-slash8s = []
+new_slash8s = []
 prefix = False
 status = False
 
@@ -40,14 +43,14 @@ def char_data(data):
     global prefix,status,new
     if prefix:
       range,_,mask = data.partition("/")
-      print "got", range, mask
+      #print "got", range, mask
       if mask == "8":
         new.append(int(range))
     elif status and new:
       if data in ["ALLOCATED", "LEGACY"]:
         if new[0] in bad:
           print data
-          slash8s.extend(new)
+          new_slash8s.extend(new)
 
 
 parser.StartElementHandler = start_element
@@ -55,12 +58,19 @@ parser.EndElementHandler = end_element
 parser.CharacterDataHandler = char_data
 
 parser.ParseFile(open("ipv4-address-space.xml"))
-print slash8s
+all_slash8s=[]
+for s8 in range(0,255):
+  if s8 not in bad or s8 in new_slash8s:
+    all_slash8s.append(s8)
+print all_slash8s
+print "(%d)" % len(all_slash8s)
+import sys
+sys.exit(0)
 import ControlScript
 # overwrite the Control script's targets
 # Only do 12 of the 13, in order to make the ControlScript's arithmetic happy.
-ControlScript.firstOctet = lambda : slash8s[:12]
-print "Not scanning ", slash8s[12:], "yet"
+ControlScript.firstOctet = lambda : new_slash8s[:12]
+print "Not scanning ", new_slash8s[12:], "yet"
 ControlScript.PORTIONS = 4
 ControlScript.MYHOST=MYHOST
 ControlScript.main()

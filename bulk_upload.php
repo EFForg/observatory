11be3db824c5b2f44973b36a825086f82093fcf0
Dan Auerbach <dan@eff.org>
2012-04-06 15:40:04 -0700

    Batch the bulk cert script

--- bulk_upload.php
+++ bulk_upload.php
@@ -6,25 +6,21 @@ require_once('config.php');
 
 $ERROR = "bulk_error.log";
 $CWD = ".";
+$BATCH_SIZE = 10000;
 //$TOTAL_CERTS
 //$BATCH_SIZE = 5;
 
 $db = new Database($c['mysql_host'], $c['mysql_username'], $c['mysql_password'], $c['mysql_database']);
 
-$query = "select raw_cert from certs limit 10000";
-$res = $db->results_query($query);
-
-
-//print count($res);
-//print_r($res);
-//exit(1);
-
-foreach ($res as $cert_arr) {
-  //$uncert = array_shift($cert_arr);
-  $cert = base64_encode($cert_arr[0]);
-  //$fp = cert_hash($cert);
-  //print "Processing ".$fp."\n";
-  $db->parse_cert($cert, $ERROR, $CWD);
+for ($i=1; $i < 32; $i++) {
+  print "Working on batch ".$i."\n";
+  $val = $i * $BATCH_SIZE;
+  $query = "select raw_cert from certs limit ".$val.", ".$BATCH_SIZE;
+  $res = $db->results_query($query);
+  foreach ($res as $cert_arr) {
+    $cert = base64_encode($cert_arr[0]);
+    $db->parse_cert($cert, $ERROR, $CWD);
+  }
 }
 
 ?>

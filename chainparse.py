37f3bab9881278528db1b28390f115a0c3d89ae7
Dan Auerbach <dan@eff.org>
2012-07-12 21:43:09 -0700

    Break out standalone validity script from chainparse

--- chainparse.py
+++ chainparse.py
@@ -4,112 +4,7 @@
 
 import sys, os, subprocess, base64
 import openssl_dump
-
-INITIAL_TRUST_PATH = './allcerts/'
-FULL_CA_TRUST_PATH = './allvalidcacerts/'
-FULL_CA_TRUST_ARGS = ['openssl', 'verify', '-CApath', FULL_CA_TRUST_PATH]
-
-
-class TransvalidityChecker(object):
-    def __init__(self, cert):
-        # the main cert to check
-        self.cert = cert
-        self._bad_fp_dict
-
-    def expandTrustRootToValidCAs(self):
-        num_added = 0
-        # get CA certs
-        ca_certs = self.getCertsFromWhereClause("`X509v3 extensions: basicConstraints` LIKE '%CA:TRUE% and Valid != 1")
-        for cert in ca_certs:
-            # is this cert valid, given existing trust root?
-            if self.verifyAgainstTrustRoot(cert):
-                self.addCertToTrustRoot(cert)
-                num_added += 1
-        return num_added
-
-    def generateTrustRoot(self):
-        while True: 
-            num = self.expandTrustRootToValidCAs()
-            if num == 0:
-                break
-            print "Added %s new CA certs to root" % num
-    
-    def verifyAgainstTrustRoot(self, cert):
-        if openssl_dump.verifyOneCert(cert, [], [], 3, True) == "Yes":
-            return True
-        else: return False
-
-    def addCertToTrustRoot(self, cert):
-        f = open('allvalidcacerts/%s' % self.getName(cert), 'w')
-        f.write(cert)
-        f.close()
-    
-    def getName(self, cert):
-        # TODO PEM->DER
-        q = "SELECT unhex(cert_fp) FROM certs WHERE cert = %s"
-
-    def getCertsFromWhereClause(self, clause):
-        q = "SELECT unhex(cert_fp), raw_cert FROM certs JOIN parsed_certs USING cert_fp WHERE %s" % clause
-        res = self.executeQuery(q)
-
-
-
-
-    def expandValidCerts(self):
-        valid_certs = self.getCertsFromWhereClause("Valid = 1")
-        subjects = self.getFieldFromCerts("Subject")
-        subjects = self.filterSelfSigned()
-
-        valid_certs = self.getValidCerts()
-        subjects = self.getSubjects(valid_certs)
-        # Get certs whose issuer is in this set of subjects
-        certs = self.getIssuers(subjects)
-        
-        
-        
-
-    def inTrustRoot(self, cert):
-        # perhaps this is unnecessary computation
-        verify = openssl_dump.verifyOneCert(cert, [], openssl_dump.ALL_VERIFY_ARGS, [], 3, True)
-        if verify == "Yes":
-            return True
-        else:
-            return False
-
-    def getFp(self, cert):
-        raise ValueError, "error not implemented"
-
-    def addCertFpToBadList(cert):
-        fp = self.getFp(cert)
-        self._bad_fp_dict[fp] = 1
-
-    def certFpInBadList(self, cert):
-        fp = self.getFp(cert)
-        if fp in self._bad_fp_dict:
-            return True
-        return False
-
-    def getDistinctIssuingCerts(self, cert):
-        # First get issuer for this certificate, assume not self-signed
-        q = "SELECT issuer FROM certs WHERE cert_fp = %s" % self.getFp(cert)
-        # TODO get result
-        q = "SELECT raw_cert FROM certs a JOIN parsed_certs b USING(cert_fp) WHERE "
-
-    def testTransvalidity(self, certlist):
-        # if anything in the list is transvalid we return true
-        # to make recursion work
-        for cert in certlist:
-            if self.certFpInBadList(cert):
-                return False
-            if self.isValid(cert):
-                return True
-            issuers = filter(lambda x: not self.certFpInBadList(x), self.getDistinctIssuingCerts(cert))
-            if not issuers:
-                self.addCertFpToBadList(cert)
-                return False
-            else:
-                return self.testTransvalidity(issuers)
-
+import dbconnect
 
 def readPemChainFromFile(fileObj):
     final = []
@@ -138,15 +33,9 @@ def readPemChainFromFile(fileObj):
     return final
 
 def checkChain(chain):
-    # todo think about timestamp -attime issue
-    #print "Chain is %s" % str(chain)
-    #print "END ENTITY CERT IS:"
-    #print chain[0]
-    #print "LAST CERT IS:"
-    #print chain[-1]
-    #verify_all = openssl_dump.verifyOneCert(chain[0], chain[1:], openssl_dump.ALL_VERIFY_ARGS, [])
     verify_moz = openssl_dump.verifyOneCert(chain[0], chain[1:], openssl_dump.MOZ_VERIFY_ARGS, [])
     verify_ms = openssl_dump.verifyOneCert(chain[0], chain[1:], openssl_dump.MS_VERIFY_ARGS, [], 3)
+    # oh boy using stdout to communicate with php is ugly...
     print verify_moz
     print verify_ms
 
---
+++ static_validity.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+# This script builds up transvalidity from the bottom up by creating
+# an expanded trust root of CA certs that are actually trusted
+# It MUST be run from the observatory git repo's root directory: observatory.
+# This is of course a horrible hack, just for now, etc etc
+
+import sys, os, subprocess, base64
+import openssl_dump
+import dbconnect
+import _mysql_exceptions
+
+# these are also hardcoded below. yuck, coding fast
+INITIAL_TRUST_PATH = './allcerts/'
+FULL_CA_TRUST_PATH = './allvalidcacerts/'
+FULL_CA_TRUST_ARGS = ['openssl', 'verify', '-CApath', FULL_CA_TRUST_PATH]
+
+class TransvalidityChecker(object):
+    def __init__(self):
+        self.gdb, self.gdbc = dbconnect.dbconnect()
+
+    def expandTrustRootToValidCAs(self):
+        num_added = 0
+        # get CA certs
+        ca_certs = self.getCertsFromWhereClause("`X509v3 extensions: basicConstraints` LIKE '%CA:TRUE%' and Valid != 1")
+        print "Found %s ostensibly invalid CA certs" % len(ca_certs)
+        for fp_hex, cert in ca_certs:
+            # is this cert valid, given existing trust root?
+            if self.verifyAgainstTrustRoot(cert):
+                self.addCertToTrustRoot(cert, fp_hex)
+                num_added += 1
+        return num_added
+
+    def generateTrustRoot(self):
+        while True: 
+            num = self.expandTrustRootToValidCAs()
+            if num == 0:
+                break
+            print "Added %s new CA certs to root" % num
+    
+    def verifyAgainstTrustRoot(self, cert):
+        if openssl_dump.verifyOneCert(cert, [], FULL_CA_TRUST_ARGS, 3, True) == "Yes":
+            return True
+        else: return False
+
+    def addCertToTrustRoot(self, cert, fp_hex):
+        # hack, must be in the right directory. todo fix this
+        filename = 'allvalidcacerts/%s.crt' % fp_hex
+        f = open(filename, 'w')
+        f.write(cert)
+        f.close()
+        self.convertToPem(filename)
+        self.rehash()
+        # also mark it valid in the db
+        q = "UPDATE parsed_certs set Valid=1 where cert_fp = unhex('%s')" % fp_hex
+        self.executeQuery(q)
+
+    def rehash():
+        subprocess.call(['c_rehash', 'allvalidcacerts'])
+
+    def getCertsFromWhereClause(self, clause):
+        q = "SELECT hex(cert_fp), raw_cert FROM certs JOIN parsed_certs USING (cert_fp) WHERE %s" % clause
+        self.executeQuery(q)
+        return self.gdbc.fetchall()
+
+    def executeQuery(self, q):
+        print "Executing: %s" % q
+        try:
+            self.gdbc.execute(q)
+        except _mysql_exceptions.OperationalError, e:
+            # if two instances of this to run at once 
+            if "Duplicate column name" in `e`:
+                # Another instance already created this column
+                return
+            raise e
+
+    def convertToPem(filename):
+        subprocess.call(['openssl', 'x509', '-in', filename, '-inform', 'der', '-outform', 'pem', '-out', filename])
+
+
+
+if __name__ == '__main__':
+    b = TransvalidityChecker()
+    b.generateTrustRoot()
